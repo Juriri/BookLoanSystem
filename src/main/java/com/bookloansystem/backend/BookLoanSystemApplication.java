77a6703b6347201cc10991e80df592a137015370
hjlee <olleh1295@gmail.com>
2023-12-07 08:26:55 +0900

    init hjlee2

--- src/main/java/com/bookloansystem/backend/BookLoanSystemApplication.java
+++ src/main/java/com/bookloansystem/backend/BookLoanSystemApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookLoanSystemApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BookLoanSystemApplication.class, args);
+        // 메모리 사용량 출력
+        long heapSize = Runtime.getRuntime().totalMemory();
+        System.out.println("HEAP Size(M) : "+ heapSize / (1024*1024) + " MB");
     }
 
 }
